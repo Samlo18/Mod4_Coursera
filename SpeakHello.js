d1571271ce141c247992e85c8397a84f3d22c8c5
Samuele <samuelelofrano18@gmail.com>
2026-03-09 21:02:39 +0100

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -13,7 +13,7 @@ o// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-  hellospeak.speak = function(name) {
+  helloSpeaker.speak = function(name) {
   console.log(speakWord + " " + name);
   };
 
